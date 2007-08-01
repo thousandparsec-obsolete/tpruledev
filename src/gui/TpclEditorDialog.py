f3538687860859ecdacb9a6e393e4905c759d3d1
James Gardner <frodough@gmail.com>
2007-08-01 15:47:01 -0700

    Started skeleton of TpclEditorDialog
    Brief outline of functionality for each button and stuffs. Introduced
    an import bug. Damn python. This has happened before and I just worked
    around it. This time I'm going to get to the bottom of things. Now that
    I've got the tpcl expression objects working (tested on the command line)
    I'll deal with the GUI tomorrow.

--- src/gui/TpclEditorDialog.py
+++ src/gui/TpclEditorDialog.py
@@ -5,6 +5,7 @@ The dialog class for the TPCL Expression Editor.
 import wx
 import gui.XrcUtilities
 from wx.xrc import XRCCTRL
+from tpcl.data import Import
 
 class Dialog(wx.Dialog):
     """
@@ -21,4 +22,55 @@ class Dialog(wx.Dialog):
         self.OnCreate()
     
     def OnCreate(self):
+        self.blocktypes = Import.LoadBlocktypes()
+        self.blocks = Import.LoadBlocks()
+        
+        #widgets
+        self.code_stc = XRCCTRL(self, "code_stc")
+        self.type_choice = XRCCTRL(self, "cat_choice")
+        self.block_list = XRCCTRL(self, "block_list")
+        self.code_stc = XRCCTRL(self, "code_stc")
+        
+        #buttons
+        self.clear_butt = XRCCTRL(self, "clear_butt")
+        self.remove_butt = XRCCTRL(self, "remove_butt")
+        self.save_butt = XRCCTRL(self, "save_butt")
+        self.insert_butt = XRCCTRL(self, "insert_butt")
+        
+        #fill the cat choice box with all blocktypes
+        
+    def OnTypeChoice(self, event):
+        """\
+        Handles a selection of a particular type
+        of TPCL code.
+        
+        We need to fill the block list with the correct
+        blocks of code.
+        """
+        
+    def OnClear(self, event):
+        """\
+        Clears the code window, deleting all unsaved
+        changes.
+        """
+        pass
+    
+    def OnRemove(self, event):
+        """\
+        Removes the current tpcl code block that is
+        selected.
+        """
+        pass
+    
+    def OnSave(self, event):
+        """\
+        Saves the current work.
+        """
+        pass
+    
+    def OnInsert(self, event):
+        """\
+        Inserts the currently selected code block
+        into the currently selected code socket.
+        """
         pass
