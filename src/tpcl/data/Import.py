29429ef2ccaa5312224d4d938ba9ff0d6485e3fb
James Gardner <frodough@gmail.com>
2007-08-17 09:42:22 -0700

    Removed unused functions

--- src/tpcl/data/Import.py
+++ src/tpcl/data/Import.py
@@ -5,71 +5,7 @@ import codegen.XmlUtils
 ET = codegen.XmlUtils.ImportElementTree()
 from tpcl.Representation import TpclBlocktype, TpclBlock, TpclTemplate
 from tpcl.BlockManagement import TpclBlockstore
-
-def LoadBlocktypes(filename=None):
-    """\
-    Returns a hash containing all of the blocktypes
-    that have been defined.
-    """
-    
-    #good golly miss molly but this code is inefficient and not fun
-    
-    if not filename:
-        from rde import ConfigManager
-        filename = os.path.join(ConfigManager.config.get('Global', 'tprde_dir'),
-                            'tpcl', 'data', 'blocktypes.xml')
-    et = ET.ElementTree(file=filename)
-    blocktypes = {}
-    for blocktype in et.findall('tpcl_blocktype'):
-        name = blocktype.get('name')
-        description = blocktype.get('description')
-        members = []
-        for member in blocktype.findall('member'):
-            members.append(member.get('name'))
-        blocktypes[name] = (TpclBlocktype(name, description), members)
-    
-    bt = {}    
-    for name, (btype, mlist) in blocktypes.iteritems():
-        for mname in mlist:
-            btype.AddMember(blocktypes[mname][0])
-        bt[name] = btype
-    del blocktypes
-    return bt
-    
-def LoadBlocks(filename=None):
-    """\
-    Returns a hash containing all of the blocks
-    that have been defined.
-    """
-    
-    if not filename:
-        from rde import ConfigManager
-        filename = os.path.join(ConfigManager.config.get('Global', 'tprde_dir'),
-                            'tpcl', 'data', 'base_blocks.xml')
-    et = ET.ElementTree(file=filename)
-    blocks = {}
-    for blocktype in et.findall('type'):
-        type = blocktype.get('name')
-        blocks[type] = {}
-        for block in blocktype.findall('tpcl_block'):
-            name = block.get('name')
-            description = block.get('description')
-            display = block.get('display')
-            tpcl_block = TpclBlock(name, type, description, display)
-            template = tpcl_block.template
-            temp = block.find('template')
-            for elem in temp.findall('elem'):
-                if elem.get('type') == "text":
-                    template.AppendTextElement(elem.get('val'))
-                elif elem.get('type') == "eol":
-                    template.AppendEolElement()
-                elif elem.get('type') == "indent":
-                    template.AppendIndentElement()
-                else:
-                    template.AppendBlockElement(elem.get('val'))
-            blocks[type][name] = (tpcl_block)
-    return blocks
-
+   
 def InitializeBlockstore(filename=None):
     if not filename:
         from rde import ConfigManager
@@ -80,10 +16,10 @@ def InitializeBlockstore(filename=None):
     bs = TpclBlockstore()
     category_stack = []
     for cat_elem in et.findall('category'):
-        BsReadCategory(cat_elem, bs, category_stack)
+        ReadCategory(cat_elem, bs, category_stack)
     return bs
         
-def BsReadCategory(element, bs, category_stack):
+def ReadCategory(element, bs, category_stack):
     cid = 0
     if len(category_stack):
         parent_id = category_stack[-1]
@@ -95,14 +31,14 @@ def BsReadCategory(element, bs, category_stack):
     category_stack.append(cid)
         
     for cat_elem in element.findall('category'):
-        BsReadCategory(cat_elem, bs, category_stack)
+        ReadCategory(cat_elem, bs, category_stack)
         
     for expr_elem in element.findall('expression'):
-        BsReadExpression(expr_elem, bs, category_stack)
+        ReadExpression(expr_elem, bs, category_stack)
         
     category_stack.pop()
     
-def BsReadExpression(expr_elem, bs, category_stack):
+def ReadExpression(expr_elem, bs, category_stack):
     name = expr_elem.get('name')
     description = expr_elem.get('description')
     if not description:
@@ -122,56 +58,6 @@ def BsReadExpression(expr_elem, bs, category_stack):
         parent_id = 0
     bs.AddBlock(tpcl_block, parent_id)
     
-def LoadBlockIntoTree(tree, filename=None):
-    """\
-    Bloody mess to only be used during testing.
-    
-    Manually fills a tree with the blocks, setting
-    PyData to the TpclBlock objects for each block
-    that we load.
-    """
-    
-    if not filename:
-        from rde import ConfigManager
-        filename = os.path.join(ConfigManager.config.get('Global', 'tprde_dir'),
-                            'tpcl', 'data', 'tpcl_base.xml')
-    et = ET.ElementTree(file=filename)
-    blocks = {}
-    root = tree.AddRoot("Root")
-    for cat_elem in et.findall('category'):
-        TreeReadCategory(cat_elem, tree)
-
-def TreeReadCategory(element, tree, parent_id=None):
-    cid = 0
-    if parent_id:
-        cid = tree.AppendItem(parent_id, element.get('name'))
-    else:
-        cid = tree.AppendItem(tree.GetRootItem(), element.get('name'))
-        
-    for cat_elem in element.findall('category'):
-        TreeReadCategory(cat_elem, tree, cid)
-        
-    for expr_elem in element.findall('expression'):
-        TreeReadExpression(expr_elem, tree, cid)
-
-def TreeReadExpression(expr_elem, tree, cat_id):
-    name = expr_elem.get('name')
-    expr_id = tree.AppendItem(cat_id, name)
-    description = expr_elem.get('description')
-    if not description:
-        description = expr_elem.findtext('multiline_desc')
-    tpcl_block = TpclBlock(name, description)
-    template_elem = expr_elem.find('template')
-    if template_elem:
-        tpcl_block.template = ReadTemplate(template_elem)
-    else:
-        raise ValueError("No template for expression: %s" % name)
-    tpcl_block.on_insert = expr_elem.findtext('oninsert')
-    tpcl_block.expansion_menu = expr_elem.findtext('expansion_menu')
-    
-    tree.SetPyData(expr_id, tpcl_block)
-    
-
 def ReadTemplate(template_elem):
     if not template_elem:
         return None
